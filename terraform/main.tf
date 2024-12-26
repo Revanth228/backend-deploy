a1248fab74a71be05c625f95b451af092d3e570e
revanth228 <devana.reddyrevanth@gmail.com>
2024-12-26 11:21:21 -0600

    backend-deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,10 @@ module "backend" {
 
   instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
-  # convert stringList to list and get first element
-
-  subnet_id     =  local.private_subnet_id
-  ami = data.aws_ami.AMI_info.id
+  # convert StringList to list and get first element
+  subnet_id = local.private_subnet_id
+  ami = data.aws_ami.ami_info.id
+  
   tags = merge(
     var.common_tags,
     {
@@ -16,68 +16,70 @@ module "backend" {
   )
 }
 
+
 resource "null_resource" "backend" {
-  triggers = {
-    instance_id = module.backend.id
-  }
+    triggers = {
+      instance_id = module.backend.id # this will be triggered everytime instance is created
+    }
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = module.backend.private_ip
-  }
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = module.backend.private_ip
+    }
 
-  provisioner "file" {
-    source      = "${var.common_tags.Component}.sh"
-    destination = "/tmp/${var.common_tags.Component}.sh"
-  }
+    provisioner "file" {
+        source      = "${var.common_tags.Component}.sh"
+        destination = "/tmp/${var.common_tags.Component}.sh"
+    }
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/${var.common_tags.Component}.sh",
-      "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component} ${var.environment} ${var.app_version}"
-    ]
-  }
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/${var.common_tags.Component}.sh",
+            "sudo sh /tmp/${var.common_tags.Component}.sh ${var.common_tags.Component} ${var.environment} ${var.app_version}"
+        ]
+    } 
 }
 
 resource "aws_ec2_instance_state" "backend" {
   instance_id = module.backend.id
   state       = "stopped"
-  depends_on  = [null_resource.backend]
+  # stop the serever only when null resource provisioning is completed
+  depends_on = [ null_resource.backend ]
 }
 
 resource "aws_ami_from_instance" "backend" {
   name               = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
   source_instance_id = module.backend.id
-  depends_on         = [aws_ec2_instance_state.backend]
+  depends_on = [ aws_ec2_instance_state.backend ]
 }
 
 resource "null_resource" "backend_delete" {
-  triggers = {
-    instance_id = module.backend.id
-  }
+    triggers = {
+      instance_id = module.backend.id # this will be triggered everytime instance is created
+    }
 
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = module.backend.private_ip
-  }
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = module.backend.private_ip
+    }
 
-  provisioner "local-exec" {
-    command = "aws ec2 terminate-instances --instance-ids ${module.backend.id}"
-  }
+    provisioner "local-exec" {
+        command = "aws ec2 terminate-instances --instance-ids ${module.backend.id}"
+    } 
 
-  depends_on = [aws_ami_from_instance.backend]
+    depends_on = [ aws_ami_from_instance.backend ]
 }
+
 
 resource "aws_lb_target_group" "backend" {
   name     = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_ssm_parameter.vpc_id.value
-
   health_check {
     path                = "/health"
     port                = 8080
@@ -94,7 +96,7 @@ resource "aws_launch_template" "backend" {
   image_id = aws_ami_from_instance.backend.id
   instance_initiated_shutdown_behavior = "terminate"
   instance_type = "t3.micro"
-  update_default_version = true
+  update_default_version = true # sets the latest version to default
 
   vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg_id.value]
 
@@ -110,6 +112,7 @@ resource "aws_launch_template" "backend" {
   }
 }
 
+
 resource "aws_autoscaling_group" "backend" {
   name                      = "${var.project_name}-${var.environment}-${var.common_tags.Component}"
   max_size                  = 5
@@ -117,14 +120,12 @@ resource "aws_autoscaling_group" "backend" {
   health_check_grace_period = 60
   health_check_type         = "ELB"
   desired_capacity          = 1
-  target_group_arns         = [aws_lb_target_group.backend.arn]
-
+  target_group_arns = [aws_lb_target_group.backend.arn]
   launch_template {
     id      = aws_launch_template.backend.id
     version = "$Latest"
   }
-
-  vpc_zone_identifier = split(",", data.aws_ssm_parameter.private_subnet_id.value)
+  vpc_zone_identifier       = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 
   instance_refresh {
     strategy = "Rolling"
@@ -160,13 +161,14 @@ resource "aws_autoscaling_policy" "backend" {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
+
     target_value = 10.0
   }
 }
 
 resource "aws_lb_listener_rule" "backend" {
   listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
-  priority     = 100
+  priority     = 100 # less number will be first validated
 
   action {
     type             = "forward"
