bbb25d5dafd6fcfa4de22afe7797533dbd51e722
revanth228 <devana.reddyrevanth@gmail.com>
2024-12-26 14:13:14 -0600

    backend-deploy

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-
 component=$1
 environment=$2
-appVersion=$3
-
+app_Version=$3
 dnf install ansible -y
-
-# Create virtual environment (optional)
-source venv/bin/activate  # Modify the path if needed
-
-pip install botocore boto3
-
+pip3.9 install botocore boto3
 ansible-pull -i localhost \
   -U https://github.com/daws-78s/expense-ansible-roles-tf.git \
   main.yaml \
-  -e env=$environment -e component=$component -e appVersion=$appVersion
-
-# Deactivate virtual environment (optional)
-deactivate
+  -e env=$environment  # Pass environment variable
+  -e component=$component -e appVersion=$appVersion  # Existing variables
