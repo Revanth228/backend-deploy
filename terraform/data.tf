d572ca90abe0dcc1b6fedf045ab8245999c5d4f3
revanth228 <devana.reddyrevanth@gmail.com>
2024-12-26 11:29:14 -0600

    backend-deploy

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,7 +2,7 @@ data "aws_ssm_parameter" "backend_sg_id" {
   name = "/${var.project_name}/${var.environment}/backend_sg_id"
 }
 
-data "aws_ssm_parameter" "private_subnet_ids" {
+data "aws_ssm_parameter" "private_subnet_id" {
   name = "/${var.project_name}/${var.environment}/private_subnet_id"
 }
 
