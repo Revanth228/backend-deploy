3302a349c0fe119eb70e1d1cc118fa4ad12bf503
revanth228 <devana.reddyrevanth@gmail.com>
2024-12-26 11:23:34 -0600

    backend-deploy

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals{
 
-    private_subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_id.value),0)
+    private_subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value),0)
 
 }
